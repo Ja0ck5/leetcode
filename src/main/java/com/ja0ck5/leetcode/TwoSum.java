3ff1bc450bdbf91f31e2c7131d7f3d77c056098a
ja0ck5 <975117619@qq.com>
2018-03-27 08:23:56 +0800

    twoSum

--- src/main/java/com/ja0ck5/leetcode/TwoSum.java
+++ src/main/java/com/ja0ck5/leetcode/TwoSum.java
@@ -26,11 +26,18 @@ public class TwoSum {
             if (map.containsKey(target - nums[i])) {
                 res[0] = map.get(target - nums[i]);
                 res[1] = i;
+                return res;
             }
             map.put(nums[i], i);
         }
         return res;
     }
 
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] res = twoSum(nums, target);
+        System.out.println(res[0] + "," + res[1]);
+    }
 
 }
