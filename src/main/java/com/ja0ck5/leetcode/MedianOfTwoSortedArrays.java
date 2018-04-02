781e5c4b5c381bbcf7fedc16cd4c28ba4f4e9aa8
ja0ck5 <975117619@qq.com>
2018-04-03 00:29:20 +0800

    findMedianSortedArrays

--- src/main/java/com/ja0ck5/leetcode/MedianOfTwoSortedArrays.java
+++ src/main/java/com/ja0ck5/leetcode/MedianOfTwoSortedArrays.java
@@ -15,6 +15,18 @@ package com.ja0ck5.leetcode;
  * nums2 = [3, 4]
  * <p>
  * The median is (2 + 3)/2 = 2.5
+ * <p>
+ * index: 0 1 2 3 4 5
+ * num1: 3 5 | 8 9             cut1 4
+ * nums2: 1 2 7 | 10 11 12     cut2 6
+ * <p>
+ * nums3: 1 2 3 5 7 | 8 9 10 11 12
+ * <p>
+ * L1 <= R2
+ * L2 <= R1
+ * <p>
+ * L1 > R2 cut1 << (cutL,cut1 - 1)
+ * L2 > R1 cut1 << (cut1 + 1,cutR)
  */
 public class MedianOfTwoSortedArrays {
 
@@ -22,6 +34,36 @@ public class MedianOfTwoSortedArrays {
         if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums1, nums2);
         }
-        return 0;
+        int cut1 = 0;
+        int cut2 = 0;
+        int cutL = 0;
+        int cutR = nums1.length;
+        int len = nums1.length + nums2.length;
+        while (cut1 <= nums1.length) {
+            cut1 = (cutR - cutL) >> 1 + cutL;
+            cut2 = len >> 1 - cut1;
+
+            double L1 = (0 == cut1) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            double L2 = (0 == cut2) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            double R1 = (nums1.length == cut1) ? Integer.MAX_VALUE : nums1[cut1];
+            double R2 = (nums2.length == cut2) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (L1 > R2) {
+                cutR = cut1 - 1;
+            } else if (L2 > R1) {
+                cutL = cut1 + 1;
+            } else {
+                if (len % 2 == 0) {
+                    L1 = L1 > L2 ? L1 : L2;
+                    R1 = R1 < R2 ? R1 : R2;
+                    return (L1 + R1) / 2;
+                } else {
+                    R1 = R1 < R2 ? R1 : R2;
+                    return R1;
+                }
+            }
+
+        }
+        return -1;
     }
 }
