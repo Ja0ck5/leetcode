aaa8af6ebc2ab4c7a9b6601533feb60b3b22807a
ja0ck5 <975117619@qq.com>
2018-04-17 00:14:21 +0800

    FourSum

---
+++ src/main/java/com/ja0ck5/leetcode/FourSum.java
@@ -0,0 +1,54 @@
+package com.ja0ck5.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums
+ * such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate quadruplets.
+ * <p>
+ * Example:
+ * <p>
+ * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+ * <p>
+ * A solution set is:
+ * [
+ * [-1,  0, 0, 1],
+ * [-2, -1, 1, 2],
+ * [-2,  0, 0, 2]
+ * ]
+ */
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 3) return res;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int l = j + 1, r = nums.length - 1;
+                while (l < r) {
+                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
+                    } else if (sum < target) {
+                        l++;
+                    } else r--;
+                }
+            }
+        }
+        return res;
+    }
+
+}
