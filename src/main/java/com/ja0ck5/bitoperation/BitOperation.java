f0e593a05e32a9caa21a3b7ebd383e80884d86b3
ja0ck5 <975117619@qq.com>
2018-07-30 15:26:22 +0800

    Bit operation

--- src/main/java/com/ja0ck5/bitoperation/BitOperation.java
+++ src/main/java/com/ja0ck5/bitoperation/BitOperation.java
@@ -1,7 +1,48 @@
 package com.ja0ck5.bitoperation;
 
+import java.util.Arrays;
+
 /**
- * 位运算 Created by Jack on 2018/7/30.
+ * 1. 原码 原码就是符号位加上真值的绝对值, 即用第一位表示符号, 其余位表示值. 比如如果是8位二进制:
+ * 
+ * [+1]原 = 0000 0001
+ * 
+ * [-1]原 = 1000 0001
+ * 
+ * 第一位是符号位. 因为第一位是符号位, 所以8位二进制数的取值范围就是:
+ * 
+ * [1111 1111 , 0111 1111]
+ * 
+ * 即
+ * 
+ * [-127 , 127]
+ * 
+ * 原码是人脑最容易理解和计算的表示方式.
+ * 
+ * 2. 反码 反码的表示方法是:
+ * 
+ * 正数的反码是其本身
+ * 
+ * 负数的反码是在其原码的基础上, 符号位不变，其余各个位取反.
+ * 
+ * [+1] = [00000001]原 = [00000001]反
+ * 
+ * [-1] = [10000001]原 = [11111110]反
+ * 
+ * 可见如果一个反码表示的是负数, 人脑无法直观的看出来它的数值. 通常要将其转换成原码再计算.
+ * 
+ * 3. 补码 补码的表示方法是:
+ * 
+ * 正数的补码就是其本身
+ * 
+ * 负数的补码是在其原码的基础上, 符号位不变, 其余各位取反, 最后+1. (即在反码的基础上+1)
+ * 
+ * [+1] = [00000001]原 = [00000001]反 = [00000001]补
+ * 
+ * [-1] = [10000001]原 = [11111110]反 = [11111111]补
+ * 
+ * 对于负数, 补码表示方式也是人脑无法直观看出其数值的. 通常也需要转换成原码在计算其数值. 位运算 Created by Jack on
+ * 2018/7/30.
  */
 public class BitOperation {
 
@@ -72,6 +113,60 @@ public class BitOperation {
 			res++;
 		}
 		return res;
+	}
+
+	/**
+	 * 在其他数都出现两次的数组中找到只出现一次的数
+	 *
+	 * 考虑异或的性质
+	 * <p>
+	 * 任何整数 n 与 0 异或总等于其本身 n，一个数与其本身异或 结果肯定是 0。
+	 * <p>
+	 * 多个数异或操作，遵循交换律和结合律。
+	 */
+	public int oddTimesNum(int[] nums) {
+		int res = 0;
+		for (int num : nums) {
+			res ^= num;
+		}
+		return res;
+	}
+
+	/**
+	 * <p>
+	 * 在其他数都出现两次的数组中找到只出现一次的 两个 数 <img src=
+	 * </p>
+	 * 任何不相同的两个除了跟自己异或外，不可能每一个位都相同, 不相同的两个数 a b 异或得到结果二进制表示上肯定有一位为 1
+	 * "http://w2.dwstatic.com/yy/ojiastoreimage/1004x960/1532922356246_11_len119656.jpg">
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int[] oddTimesNums(int[] nums) {
+
+		int[] res = new int[2];
+		int i = 0, j = 0;
+		for (int n : nums) {
+			i ^= n;
+		}
+		// 与本身补码相与
+		int rightOne = i & (~i + 1);
+		for (int n : nums) {
+			// 相与等于 0 则代表了这个数肯定是两个数中第 k 位不为 1 的那个
+			if ((rightOne & n) != 0) {
+				j ^= n;
+			}
+		}
+		res[0] = j;
+		res[1] = j ^ i;
+		return res;
+	}
+
+	public static void main(String[] args) {
+		BitOperation bo = new BitOperation();
+		int[] nums = { 1, 2, 3, 4, 1, 2, 3, 5 };
+		int[] res = bo.oddTimesNums(nums);
+		System.out.println(Arrays.toString(res));
 	}
 
 }
