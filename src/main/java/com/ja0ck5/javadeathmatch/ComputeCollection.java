73b7b65a1620947d93092e60521b8c88850a90b3
ja0ck5 <975117619@qq.com>
2018-09-10 13:11:33 +0800

    ComputeCollection

--- src/main/java/com/ja0ck5/javadeathmatch/ComputeCollection.java
+++ src/main/java/com/ja0ck5/javadeathmatch/ComputeCollection.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Logger;
 
 /**
  * @author: Ja0ck5
@@ -14,7 +17,7 @@ import java.util.TreeMap;
 public class ComputeCollection {
 
 	public static void main(String[] args) {
-        Map<String, Object> collection = new TreeMap<>();
+		Map<String, Object> collection = new TreeMap<>();
 
 		System.out.println(
 				collection.compute("foo", (k, v) -> (v == null) ? new ArrayList<Object>() : ((List) v).add("bar")));
@@ -23,4 +26,13 @@ public class ComputeCollection {
 
 	}
 
+	public static <T> T retrieveFutureResult(Future<T> future) throws InterruptedException {
+		try {
+			return future.get();
+		} catch (ExecutionException ex) {
+			Exception sourceException = (Exception) ex.getCause();
+			Logger.getGlobal().throwing("Main", "retrieveFutureResult", sourceException);
+			return null;
+		}
+	}
 }
