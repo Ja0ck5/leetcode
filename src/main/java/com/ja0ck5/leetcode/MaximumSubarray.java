7d6724834a4b3b1f66be0a4bed8efe44a5658d78
ja0ck5 <975117619@qq.com>
2019-01-03 10:03:17 +0800

    MaximumSubarray TODO dc

--- src/main/java/com/ja0ck5/leetcode/MaximumSubarray.java
+++ src/main/java/com/ja0ck5/leetcode/MaximumSubarray.java
@@ -54,4 +54,6 @@ public class MaximumSubarray {
 		return res;
 	}
 
+	// TODO If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
 }
