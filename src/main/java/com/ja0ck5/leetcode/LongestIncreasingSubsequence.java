764502eea472392cfa3c17652dc0e0f78896be0a
ja0ck5 <975117619@qq.com>
2019-01-21 11:13:14 +0800

    LongestIncreasingSubsequence

--- src/main/java/com/ja0ck5/leetcode/LongestIncreasingSubsequence.java
+++ src/main/java/com/ja0ck5/leetcode/LongestIncreasingSubsequence.java
@@ -22,11 +22,12 @@ package com.ja0ck5.leetcode;
  */
 public class LongestIncreasingSubsequence {
 
-    /**
-     * dp O(n^2)
-     * @param nums
-     * @return
-     */
+	/**
+	 * dp O(n^2)
+	 * 
+	 * @param nums
+	 * @return
+	 */
 	public int lengthOfLIS(int[] nums) {
 		if (nums == null || nums.length == 0)
 			return 0;
@@ -47,18 +48,47 @@ public class LongestIncreasingSubsequence {
 		return ans;
 	}
 
-    /**
-     *
-     * O(nlogn)
-     * @param nums
-     * @return
-     */
-    public int lengthOfLIS2(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return 0;
-        int ans = 0;
+	/**
+	 *
+	 * O(nlogn)
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int lengthOfLIS2(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+		int[] minLast = new int[nums.length + 1];
+		minLast[0] = Integer.MIN_VALUE;
+		for (int i = 1; i <= nums.length; i++) {
+			minLast[i] = Integer.MAX_VALUE;
+		}
 
-        return ans;
-    }
+		for (int i = 0; i < nums.length; i++) {
+			// find the first number in minLast >= nums[i]
+			int index = binarySearch(minLast, nums[i]);
+			minLast[index] = nums[i];
+		}
+
+		for (int i = nums.length; i >= 1; i--) {
+			if (minLast[i] != Integer.MAX_VALUE)
+				return i;
+		}
+
+		return 0;
+	}
+
+	private int binarySearch(int[] minLast, int num) {
+		int start = 0, end = minLast.length - 1;
+		while (start + 1 < end) {
+			int mid = (end - start) / 2 + start;
+			if (minLast[mid] < num) {
+				start = mid;
+			} else {
+				end = mid;
+			}
+		}
+		return end;
+	}
 
 }
