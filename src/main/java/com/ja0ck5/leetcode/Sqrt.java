21bbf981a7752549e703af9ea0a551ab282dfd7a
ja0ck5 <975117619@qq.com>
2019-04-08 10:38:23 +0800

    Sqrt

---
+++ src/main/java/com/ja0ck5/leetcode/Sqrt.java
@@ -0,0 +1,60 @@
+package com.ja0ck5.leetcode;
+
+/**
+ *
+ * 69. Sqrt(x)
+ * Easy
+ *
+ * 695
+ *
+ * 1272
+ *
+ * Favorite
+ *
+ * Share
+ * Implement int sqrt(int x).
+ *
+ * Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+ *
+ * Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+ *
+ * Example 1:
+ *
+ * Input: 4
+ * Output: 2
+ * Example 2:
+ *
+ * Input: 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since
+ *              the decimal part is truncated, 2 is returned.
+ *
+ * @author: Ja0ck5
+ * @Description:
+ * @Date: Created in 10:22 2019/4/8
+ * @Modified By:
+ */
+public class Sqrt {
+
+    public int mySqrt(int x) {
+        int lo = 0;
+        int hi = x;
+        int mid;
+        while (lo <= hi){
+            mid = (hi - lo)/2 + lo;
+            if(mid * mid == x){
+                return mid;
+            }else if(mid * mid < x){
+                lo = mid + 1;
+            }else{
+                hi = mid - 1;
+            }
+        }
+        if(hi * hi < x){
+            return hi;
+        }else{
+            return lo;
+        }
+    }
+
+}
